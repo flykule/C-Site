1f6309f6b08165812b261c4a969bb81b1f30acd3
castle <castleliu40@gmail.com>
2016-09-21 18:17:26 +0800

    修bug

--- app/src/main/java/com/example/castle/csite/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/castle/csite/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.castle.csite.ui.activity;
 
+import com.example.castle.csite.ui.base.BaseActivity;
+
 /**
  * Created by castle on 16-9-21.
  */
-public class MainActivity {
+public class MainActivity extends BaseActivity {
 }
