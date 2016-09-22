47e5d2597c91c43da3a156e98ca87cba2e823a76
castle <castleliu40@gmail.com>
2016-09-22 19:30:24 +0800

    首页以及侧滑栏实现

---
+++ app/src/main/java/com/example/castle/csite/ui/activity/InterestActivity.java
@@ -0,0 +1,21 @@
+package com.example.castle.csite.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.castle.csite.R;
+import com.example.castle.csite.ui.base.BaseActivity;
+import com.example.castle.csite.view.BindLayout;
+
+/**
+ * @author 灰太狼
+ * @time 2016/9/22  17:07
+ * @desc 兴趣圈页面
+ */
+@BindLayout(id = R.layout.activity_interest)
+public class InterestActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
