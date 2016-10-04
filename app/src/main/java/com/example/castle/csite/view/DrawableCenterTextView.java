66ffed22df5ef589cb34d7fd76d42f03d39d5594
castle <castleliu40@gmail.com>
2016-10-04 17:48:35 +0800

    添加进度条图片2

---
+++ app/src/main/java/com/example/castle/csite/view/DrawableCenterTextView.java
@@ -0,0 +1,49 @@
+package com.example.castle.csite.view;
+
+/**
+ * Created by castle on 16-10-4.
+ */
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * drawableLeft与文本一起居中显示
+ *
+ *
+ */
+public class DrawableCenterTextView extends TextView {
+
+    public DrawableCenterTextView(Context context, AttributeSet attrs,
+                                  int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public DrawableCenterTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DrawableCenterTextView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Drawable[] drawables = getCompoundDrawables();
+        if (drawables != null) {
+            Drawable drawableLeft = drawables[0];
+            if (drawableLeft != null) {
+                float textWidth = getPaint().measureText(getText().toString());
+                int drawablePadding = getCompoundDrawablePadding();
+                int drawableWidth = 0;
+                drawableWidth = drawableLeft.getIntrinsicWidth();
+                float bodyWidth = textWidth + drawableWidth + drawablePadding;
+                canvas.translate((getWidth() - bodyWidth) / 2, 0);
+            }
+        }
+        super.onDraw(canvas);
+    }
+}
