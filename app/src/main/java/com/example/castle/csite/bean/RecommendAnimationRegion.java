adc75fa4188b1f1374fd08d880b744e02b1485ea
castle <castleliu40@gmail.com>
2016-10-08 21:57:07 +0800

    推荐页面ui完成大部分,添加头

---
+++ app/src/main/java/com/example/castle/csite/bean/RecommendAnimationRegion.java
@@ -0,0 +1,230 @@
+package com.example.castle.csite.bean;
+
+import java.util.List;
+
+/**
+ * Created by castle on 16-10-8.
+ */
+
+public class RecommendAnimationRegion {
+
+    /**
+     * list : [{"aid":2915613,"typeid":27,"title":"【德国骨科】这对不能拆，小埋和哥哥结婚以后","subtitle":"","play":688909,"review":1243,"video_review":9087,"favorites":23882,"mid":5676753,"author":"亲爹说","description":"自制 既然你们已经看过很多版本的狐狸精X小埋了，那么就来看看小埋老婆在家装死吧。 我是莱克斯撸SIR，专注为德国骨科揽活二十年，撮合兄妹无数。你们再胡乱拆散哥哥和小埋，信不信我把阳平和芽衣撮合成一对？","create":"2015-09-16 19:03","pubdate":"1442401418","pic":"http://i2.hdslb.com/320_200/video/d9/d96916cfe1401f040e168d0517751102.jpg","credit":0,"coins":17976,"duration":"2:58"},{"aid":4500097,"typeid":24,"title":"【真·预告向/驱魔少年】亚连君 欢迎回来~庆祝驱魔TV回归！","subtitle":"","play":7517,"review":51,"video_review":771,"favorites":260,"mid":3551108,"author":"漆夭71","description":"自制 看到驱魔TV版7月回归了，真是有生之年系列，一直很喜欢驱魔，于是就想做个预告向！欢迎亚连小天使回家hhh 希望这个MAD能让驱魔厨们再燃一波！期待第二季ww BGM:澤野弘之-Tragedy 小林沙苗-つないだ手にキスを up主:漆夭71君","create":"2016-04-29 15:13","pubdate":"1461914029","pic":"http://i1.hdslb.com/bfs/archive/d02f536a5a7a92aa9f3d8f105965e396510da59e.jpg_320x200.jpg","credit":0,"coins":237,"duration":"4:00"},{"aid":6547504,"typeid":27,"title":"《在下坂本，有何贵干？》第13话中文字幕","subtitle":"","play":40384,"review":0,"video_review":1422,"favorites":2782,"mid":8360100,"author":"神圣种菜者","description":"《在下坂本，有何贵干？》第13话 给加了中文字幕 视频本身好像不全的样子，等我找到全的，高清的在做一次好了。","create":"2016-10-05 14:42","pubdate":"1475649756","pic":"http://i2.hdslb.com/bfs/archive/7d12a6f72222ff1a7dcd0f9834cd33cd9e80f397.jpg_320x200.jpg","credit":0,"coins":529,"duration":"9:35"},{"aid":2673621,"typeid":53,"title":"【弹幕大赛】WIN95屏幕保护程序3D迷宫附游戏（完成）","subtitle":"","play":1212352,"review":4114,"video_review":510407,"favorites":12307,"mid":444224,"author":"多鲁基X铃","description":"自制 1P屏幕保护程序，2P为1P对应的游戏版，3P为稍微难一点的迷宫游戏。游戏为WAD键操作，游戏和屏保都内置彩蛋，所有彩蛋均可点击。鼓励大家收集彩蛋，请注意，只有迷宫死路尽头的彩蛋可以收集，收集方法是按W键。视频弹幕会被吞所以请有效利用评论区。那么问题来了，迷宫中都出现了哪些UP主的哪些作品？","create":"2015-08-04 09:47","pubdate":"1438652862","pic":"http://i1.hdslb.com/320_200/video/07/07129c8f31b24b7b07a2787443a3b584.jpg","credit":0,"coins":1375,"duration":"66:37"},{"aid":6494596,"typeid":27,"title":"【补番推荐】那些被誉为神作的动画，你看过几个？","subtitle":"","play":218102,"review":28,"video_review":40052,"favorites":16981,"mid":432230,"author":"雪和风","description":"自制 本次为大家整理推荐的是大部分宅们所认同的神作动画中 我所认同的神作，算是个人向吧 都是需要一定心理承受能力观看的，最好慎重选择哦","create":"2016-10-02 09:31","pubdate":"1475371912","pic":"http://i2.hdslb.com/bfs/archive/fd80f1c2c7e6c967f235537a91fab4d89d27a5fb.jpg_320x200.jpg","credit":0,"coins":4327,"duration":"25:05"},{"aid":6214487,"typeid":27,"title":"【何必推荐呢】后宫漫福利推荐第二弹 你们猜猜我是什么时候做的视频~","subtitle":"","play":229416,"review":5,"video_review":7382,"favorites":18252,"mid":2829782,"author":"何必Hebee","description":"自制 我敢打赌没有一个人在生日那天在做什么后宫漫推荐的视频 mdzz 我也是下午才知道的 所以本来想明天发那就提前一天吧 大家见过无数up主生日发视频 但是绝对没人发后宫推荐视频 算了 祝何必生日快乐 希望明年这时候我还在发视频 你们还在这里","create":"2016-09-09 19:49","pubdate":"1473421771","pic":"http://i2.hdslb.com/bfs/archive/62eeee4ebdd41ac66359bb11ccecf1af69d4e563.jpg_320x200.jpg","credit":0,"coins":5616,"duration":"18:31"},{"aid":6597472,"typeid":27,"title":"【宅语杂谈】简评《你的名字》-震撼心灵的美？","subtitle":"","play":226,"review":0,"video_review":20,"favorites":8,"mid":718750,"author":"七海琉璃ちゃん","description":"最近《你的名字》异常的火热，其实在电影首映之前我就预定想做的 但其开播后影响力比我想象中要大的多，甚至成为了一大热点 我这人不太喜欢去刷热点话题，于是犹豫了 嘛...因为事先夸下太多海口，通知了很多人，也没法停下 所以我就简评几句吧！ 啊呸！发觉自己有点矫情了！ 恩...最近尝试做的也差不多了，玩也玩够了，下期差不多来要一发正式的吧！ by阿拉垃圾垃圾君","create":"2016-10-08 16:25","pubdate":"1475915101","pic":"http://i0.hdslb.com/bfs/archive/61580119de2839f92ed71deaf4fb4ae5c40c9144.jpg_320x200.jpg","credit":0,"coins":11,"duration":"5:40"},{"aid":6457793,"typeid":47,"title":"【动画】We Want To Tell U A Story（预告）","subtitle":"","play":244442,"review":14,"video_review":3218,"favorites":17324,"mid":1320347,"author":"遗魂儿","description":"自制 丨新浪微博：@嘿煤窑 丨STAFF详情见评论（请务必帮忙顶上热评，简介限制250字放不下QWQ） 丨00:20s之前音乐节选自《Jeff Williams - Fall》，其余音乐均为本制作组原创音乐 丨感谢所有制作成员dalao，能一起合作出产这部平时想都不敢想的企划 丨同时也感谢一直以来支持我们的朋友们，本次预告算是二周年给大家的献礼吧（虽然好像有点迟到了0v0） 丨官博已经正式上线，今后可以通过关注官博了解第四期进度更（wei）新（xing）以及其他周边作品","create":"2016-09-29 14:59","pubdate":"1475132363","pic":"http://i2.hdslb.com/bfs/archive/26c265182d25ce72f0b4ff13726a67b5c4a20c5c.jpg_320x200.jpg","credit":0,"coins":27167,"duration":"1:30"},{"aid":3777253,"typeid":47,"title":"【月歌。手书】Rolling girl【不停翻转的12--5月】","subtitle":"","play":866,"review":6,"video_review":60,"favorites":125,"mid":1955010,"author":"defzsit","description":"sm23694225 看着就疼的Rolling girl【boy】...","create":"2016-02-07 11:21","pubdate":"1454815310","pic":"http://i2.hdslb.com/320_200/u_user/68c51bc7cf28e1bf000871839e04e8ea.jpg","credit":0,"coins":20,"duration":"2:13"},{"aid":4071887,"typeid":24,"title":"【火影/鸣佐】原来你是我最想留住的幸运","subtitle":"","play":39058,"review":262,"video_review":970,"favorites":2478,"mid":1750332,"author":"gracesting","description":"自制 BGM：田馥甄-小幸运 up主纯新人 不足之处请指出w 中间有GN说出现鼬佐情节我来解释一下，其实就是为了对应歌词啊，佐助杀了鼬以后才后知后觉么。不是出现鼬就是鼬佐好么\u2026\u2026 还有没有修改的第一版在2p 看有的弹幕说到更喜欢第一版就搬上来了 虽然我并没有觉得两个有什么太大差别【手动再见 ","create":"2016-03-12 01:06","pubdate":"1457716019","pic":"http://i2.hdslb.com/bfs/archive/15fae265134cfe42edff5119e8eae6e13ce0c2e0.jpg_320x200.jpg","credit":0,"coins":1174,"duration":"4:22"},{"aid":6554340,"typeid":27,"title":"这个空耳真厉害！第三期","subtitle":"","play":303222,"review":6,"video_review":15178,"favorites":18202,"mid":14292748,"author":"空耳组","description":"自制 《这个空耳真厉害！》是由【空耳组】推出的空耳期刊，是一个把最新最好的ACG音乐空耳呈现给大家的平台。我们的口号是：好空耳一直都在！ 素材范围为最近半年内新出的ACG相关音乐。投稿请私信@空耳组。对空耳有兴趣的朋友、想参与到本刊制作中来的朋友，欢迎加入空耳兴趣交流QQ群531034534。 微博【空耳组】，微信【空耳南泊湾】欢迎关注！","create":"2016-10-05 21:37","pubdate":"1475674641","pic":"http://i1.hdslb.com/bfs/archive/8159fb0d07ff285f66298fd058a639178b8a4993.jpg_320x200.jpg","credit":0,"coins":19875,"duration":"19:43"},{"aid":6469990,"typeid":27,"title":"Miu平方第八期 macross delta宇宙K歌房神曲全回顾（假的，其实是正经作曲人科普","subtitle":"","play":271,"review":0,"video_review":121,"favorites":4,"mid":25517049,"author":"次元矩阵","description":"这期节目正经得我都不好意思。","create":"2016-09-30 16:49","pubdate":"1475225358","pic":"http://i0.hdslb.com/bfs/archive/b900ef8f7cfea6b529777ce8a8332485eabcd700.jpg_320x200.jpg","credit":0,"coins":4,"duration":"16:14"},{"aid":5327022,"typeid":24,"title":"【炮姐/MAD/弹幕高能】超电磁炮-bilibili 永世长存","subtitle":"","play":5575,"review":29,"video_review":346,"favorites":268,"mid":12936029,"author":"一只带电的喵","description":"自制，bgm:Fermata~Akkord-Fortissimo~ fripSide,素材：某科学的超电磁炮，某科学的超电磁炮's，魔法禁书目录，感谢在制作道路上\u201c吾名北斗请多指教\u201d的真诚帮助，也感谢大家的观看。有什么建议可以在评论提出，希望大家喜欢，也感谢高级弹幕君的支持","create":"2016-07-14 12:24","pubdate":"1468470241","pic":"http://i0.hdslb.com/bfs/archive/38e311a324d3ff6c2bbc4d40953f9aa70649bce9.jpg_320x200.jpg","credit":0,"coins":110,"duration":"3:16"},{"aid":6534956,"typeid":27,"title":"动漫中那些主角帅炸天的场景 第二期","subtitle":"","play":44722,"review":0,"video_review":1100,"favorites":1073,"mid":7128306,"author":"红う世","description":"在昨晚第一期之后就想做第二期了，奈何素材还在更新中，最近有时间终于做完了，希望大家支持。纯洁的UP主参上！！！","create":"2016-10-04 19:00","pubdate":"1475578856","pic":"http://i0.hdslb.com/bfs/archive/fe9aadacae3ee769274f660f0cbfa6f58b5fd3c3.jpg_320x200.jpg","credit":0,"coins":319,"duration":"21:10"},{"aid":6541231,"typeid":27,"title":"【强】论B站的观众最喜欢看谁的动画","subtitle":"","play":86055,"review":1,"video_review":10149,"favorites":4889,"mid":10005407,"author":"摇曳百合吧","description":"STAFF 文案：@追梦の记忆 @OPandED君 数据：@jyhloving 顾问：@YuMik0玉米 B站的观众最喜欢看哪家公司的动画：av3955246 ","create":"2016-10-05 01:41","pubdate":"1475602891","pic":"http://i0.hdslb.com/bfs/archive/8c3cd12511e1ec38d539f67ca56ab709189cfc6d.jpg_320x200.jpg","credit":0,"coins":5506,"duration":"27:09"},{"aid":6559941,"typeid":27,"title":"[排行向]Cup主最爱的二次元女角排行","subtitle":"","play":96998,"review":12,"video_review":20170,"favorites":3919,"mid":113711,"author":"疾風醬","description":"720P 我喜欢Bilibili,更爱二次元!!曲+图包:http://pan.baidu.com/s/1pKSjo99","create":"2016-10-06 09:51","pubdate":"1475718707","pic":"http://i0.hdslb.com/bfs/archive/f1abe9441c29118c4b4e6f782ddc68f319fb6f33.jpg_320x200.jpg","credit":0,"coins":3694,"duration":"12:07"},{"aid":2532168,"typeid":24,"title":"【排球少年】「Volleybot」","subtitle":"","play":77649,"review":259,"video_review":1615,"favorites":4508,"mid":4945659,"author":"Kiwilder","description":"YouTube \u203b机器人设定\u203b「Japan Expo 2015 AMV France」「Editor: Celia Phantomhive」「封面id: 50999776」「BGM: Fiz &amp; the Tantrums - The Walker」「https://www.youtube.com/watch?v=qjXH7cneK2c」","create":"2015-07-06 07:46","pubdate":"1436139961","pic":"http://i1.hdslb.com/320_200/video/a7/a787cd57233448104f306d1679cfd25e.jpg","credit":0,"coins":1703,"duration":"4:20"},{"aid":6592175,"typeid":27,"title":"7月完结总评推荐（残）","subtitle":"","play":214,"review":0,"video_review":6,"favorites":8,"mid":13097929,"author":"喵辞书的放牧人由理子","description":"你们也看到了残的，有够短的，真是做的非常不满意","create":"2016-10-08 02:37","pubdate":"1475865472","pic":"http://i2.hdslb.com/bfs/archive/8c4f99ed3448d47f3d8af9cd8c226586f0d362c3.jpg_320x200.jpg","credit":0,"coins":15,"duration":"13:38"},{"aid":6182033,"typeid":24,"title":"【多素材/燃向】纯白的世界 绝望的黑色","subtitle":"","play":9849,"review":0,"video_review":129,"favorites":546,"mid":7346847,"author":"天江伊","description":"自制：纯白的你 黑色的我 即使不同世界依旧在一起 BGM：Sixx# A.M. - This is Gonna Hurt","create":"2016-09-06 21:22","pubdate":"1473168146","pic":"http://i2.hdslb.com/bfs/archive/a63ddd4ce89e08ee0d0aceff8f4be73fdcb9de6a.jpg_320x200.jpg","credit":0,"coins":126,"duration":"3:28"},{"aid":2429438,"typeid":24,"title":"【Fate/史诗向/AMV】奇迹的传承","subtitle":"","play":250439,"review":1085,"video_review":2815,"favorites":11188,"mid":7157208,"author":"卡莲の圣骸布","description":"BGM:Empire Of Angels 自制,算是第一次自己做的真正的作品吧,讲述那个奇迹的许愿杯下战斗的芸芸众生们 素材：Menu01-05,BDpv1-3 纯剪辑 加入了BGM和字幕","create":"2015-06-12 16:13","pubdate":"1434096787","pic":"http://i1.hdslb.com/320_200/video/a3/a38a23ede0359a6bc0a414db31c231c1.jpg","credit":0,"coins":2244,"duration":"4:17"}]
+     * results : 20
+     * pages : 0
+     * code : 0
+     */
+
+    private int results;
+    private int pages;
+    private int code;
+    /**
+     * aid : 2915613
+     * typeid : 27
+     * title : 【德国骨科】这对不能拆，小埋和哥哥结婚以后
+     * subtitle :
+     * play : 688909
+     * review : 1243
+     * video_review : 9087
+     * favorites : 23882
+     * mid : 5676753
+     * author : 亲爹说
+     * description : 自制 既然你们已经看过很多版本的狐狸精X小埋了，那么就来看看小埋老婆在家装死吧。 我是莱克斯撸SIR，专注为德国骨科揽活二十年，撮合兄妹无数。你们再胡乱拆散哥哥和小埋，信不信我把阳平和芽衣撮合成一对？
+     * create : 2015-09-16 19:03
+     * pubdate : 1442401418
+     * pic : http://i2.hdslb.com/320_200/video/d9/d96916cfe1401f040e168d0517751102.jpg
+     * credit : 0
+     * coins : 17976
+     * duration : 2:58
+     */
+
+    private List<ListBean> list;
+
+    public int getResults() {
+        return results;
+    }
+
+    public void setResults(int results) {
+        this.results = results;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public List<ListBean> getList() {
+        return list;
+    }
+
+    public void setList(List<ListBean> list) {
+        this.list = list;
+    }
+
+    public static class ListBean {
+        private int aid;
+        private int typeid;
+        private String title;
+        private String subtitle;
+        private int play;
+        private int review;
+        private int video_review;
+        private int favorites;
+        private int mid;
+        private String author;
+        private String description;
+        private String create;
+        private String pubdate;
+        private String pic;
+        private int credit;
+        private int coins;
+        private String duration;
+
+        public int getAid() {
+            return aid;
+        }
+
+        public void setAid(int aid) {
+            this.aid = aid;
+        }
+
+        public int getTypeid() {
+            return typeid;
+        }
+
+        public void setTypeid(int typeid) {
+            this.typeid = typeid;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getSubtitle() {
+            return subtitle;
+        }
+
+        public void setSubtitle(String subtitle) {
+            this.subtitle = subtitle;
+        }
+
+        public int getPlay() {
+            return play;
+        }
+
+        public void setPlay(int play) {
+            this.play = play;
+        }
+
+        public int getReview() {
+            return review;
+        }
+
+        public void setReview(int review) {
+            this.review = review;
+        }
+
+        public int getVideo_review() {
+            return video_review;
+        }
+
+        public void setVideo_review(int video_review) {
+            this.video_review = video_review;
+        }
+
+        public int getFavorites() {
+            return favorites;
+        }
+
+        public void setFavorites(int favorites) {
+            this.favorites = favorites;
+        }
+
+        public int getMid() {
+            return mid;
+        }
+
+        public void setMid(int mid) {
+            this.mid = mid;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getCreate() {
+            return create;
+        }
+
+        public void setCreate(String create) {
+            this.create = create;
+        }
+
+        public String getPubdate() {
+            return pubdate;
+        }
+
+        public void setPubdate(String pubdate) {
+            this.pubdate = pubdate;
+        }
+
+        public String getPic() {
+            return pic;
+        }
+
+        public void setPic(String pic) {
+            this.pic = pic;
+        }
+
+        public int getCredit() {
+            return credit;
+        }
+
+        public void setCredit(int credit) {
+            this.credit = credit;
+        }
+
+        public int getCoins() {
+            return coins;
+        }
+
+        public void setCoins(int coins) {
+            this.coins = coins;
+        }
+
+        public String getDuration() {
+            return duration;
+        }
+
+        public void setDuration(String duration) {
+            this.duration = duration;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/castle/csite/util/RxBus.java
@@ -0,0 +1,73 @@
+package com.example.castle.csite.util;
+
+import rx.Observable;
+import rx.subjects.PublishSubject;
+import rx.subjects.SerializedSubject;
+import rx.subjects.Subject;
+
+/**
+ * Created by castle on 16-10-8.
+ * 自定义RxBus用于取代EventBus
+ */
+
+public class RxBus {
+    private static volatile RxBus mInstance;
+
+    private final Subject bus;
+
+
+    public RxBus()
+    {
+        bus = new SerializedSubject<>(PublishSubject.create());
+    }
+
+    /**
+     * 单例模式RxBus
+     *
+     * @return
+     */
+    public static RxBus getInstance()
+    {
+
+        RxBus rxBus2 = mInstance;
+        if (mInstance == null)
+        {
+            synchronized (RxBus.class)
+            {
+                rxBus2 = mInstance;
+                if (mInstance == null)
+                {
+                    rxBus2 = new RxBus();
+                    mInstance = rxBus2;
+                }
+            }
+        }
+
+        return rxBus2;
+    }
+
+
+    /**
+     * 发送消息
+     *
+     * @param object
+     */
+    public void post(Object object)
+    {
+
+        bus.onNext(object);
+
+    }
+
+    /**
+     * 接收消息
+     *
+     * @param eventType
+     * @param <T>
+     * @return
+     */
+    public <T> Observable<T> toObserverable(Class<T> eventType)
+    {
+        return bus.ofType(eventType);
+    }
+}
